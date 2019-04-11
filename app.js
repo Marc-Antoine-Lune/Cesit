d6cf75b0fba1fdf192ed81f498b35c7a5874e736
Marc-Antoine-Lune <41789916+Marc-Antoine-Lune@users.noreply.github.com>
2019-04-11 17:12:55 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 function init(){
-    var n = 180; // Nombre final du compteur
+    var n = 200; // Nombre final du compteur
     var cpt = 0; // Initialisation du compteur
     var duree = 5; // Durée en seconde pendant laquel le compteur ira de 0 à 15
     var delta = Math.ceil((duree * 1000) / n); // On calcule l'intervalle de temps entre chaque rafraîchissement du compteur (durée mise en milliseconde)
     var node =  document.getElementById("compteur"); // On récupère notre noeud où sera rafraîchi la valeur du compteur
     
     function countdown() {
-    node.innerHTML = ++cpt*60;
+    node.innerHTML = ++cpt*50;
     if( cpt < n ) { // Si on est pas arrivé à la valeur finale, on relance notre compteur une nouvelle fois
         setTimeout(countdown, 0.05);
     }
